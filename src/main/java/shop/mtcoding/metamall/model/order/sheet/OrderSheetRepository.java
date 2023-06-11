5e669191e0c979f1fb08e32bdeaa1e01266aa2c3
leek94 <khwan929@naver.com>
2023-06-12 03:06:55 +0900

    feat: 레포지토리 설계 완료

--- src/main/java/shop/mtcoding/metamall/model/order/sheet/OrderSheetRepository.java
+++ src/main/java/shop/mtcoding/metamall/model/order/sheet/OrderSheetRepository.java
@@ -1,6 +1,13 @@
 package shop.mtcoding.metamall.model.order.sheet;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface OrderSheetRepository extends JpaRepository<OrderSheet, Long> {
+    //내가 주문한 목록보기
+    @Query("select os from OrderSheet os where os.user.id = :userId")
+    List<OrderSheet> findByUserId(@Param("userId") Long userId);
 }
